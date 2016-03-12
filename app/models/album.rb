246a1a0d16ccf70563051111cfb3a1d753ab50d4
Surajit Khan <surajit16@gmail.com>
2016-03-12 17:39:13 +0530

    6. Add code to ensure that when an album is saved, a publisher is required.

--- app/models/album.rb
+++ app/models/album.rb
@@ -3,6 +3,7 @@ class Album < ActiveRecord::Base
   has_many :songs
   
   validates :name, presence: true, length: { in: 2..50 }
+  validates :publisher_id, presence: true
   validates :cover_art, presence: true
   validates :released_on, presence: true, format: { with: /\A\d{4}-\d{2}-\d{2}\z/, message: "should be in the format YYYY-MM-DD" }
 end
